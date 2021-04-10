56d95047bbff8c151aa88bd7c7af3c647fe5ca42
Michael Holmes <michaelholmes618@gmail.com>
2021-04-10 12:24:08 -0500

    baseball

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 //It should display a h1 heading.
 
-ReactDOM.render(
-  <h1>Hello Sly and Hershey</h1>,
-  document.getElementById("root")
-);
 //It should display an unordered list (bullet points).
 
 //It should contain 3 list elements.
@@ -14,11 +10,11 @@ const list = "<ul><li>Feed Me</li><li>Nap Time</li><li>No Baths</li></ul>";
 
 ReactDOM.render(
   <div>
-    <h1>Hello Sly and Hershey</h1>
+    <h1>My Top College Baseball Teams</h1>
     <ul>
-      <li>Feed Me</li>
-      <li>Nap Time</li>
-      <li>No Baths</li>
+      <li>Arkansas</li>
+      <li>Vanderbilt</li>
+      <li>Mississippi State</li>
     </ul>
   </div>,
   document.getElementById("root")
